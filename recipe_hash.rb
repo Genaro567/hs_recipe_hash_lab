fae65f7895fb713b25064a135b7ed521ca248bf8
twant <taylor.want@gmail.com>
2016-11-01 21:43:45 +0000

    remove .rspec and .learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -9,32 +9,35 @@
 
 
 #2. Write a method that returns (not puts) your mac_and_cheese_ hash
-def recipe_ingredients(mac_and_cheese_recipe)
+def recipe_ingredients(any_recipe)
 
 end
 
+puts recipe_ingredients(mac_and_cheese_recipe)
+
+
 #2: Write code that returns the amount of cheese from mac_and_cheese ingredients that is needed in the recipe.
-def amount_of_cheese(mac_and_cheese_recipe)
+def amount_of_cheese(any_recipe)
 
 end
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
-def ingredients_and_amounts(mac_and_cheese_recipe)
+def ingredients_and_amounts(any_recipe)
  
 end
 
 #4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
-def ingredients(mac_and_cheese_recipe)
+def ingredients(any_recipe)
  
 end
 
 #5: Now use `puts` to print out just the amounts without the ingredients. You'll want to use the `each_value` method.
-def amounts(mac_and_cheese_recipe)
+def amounts(any_recipe)
 
 end
 
 #6: Return the number of ingredients in the `mac_and_cheese_recipe` hash.
-def number_ingredients(mac_and_cheese_recipe)
+def number_ingredients(any_recipe)
 
 end
 
